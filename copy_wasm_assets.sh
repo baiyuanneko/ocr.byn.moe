85794bd4bfa3476fb4aef7a8c2d06352515d6b96
柏园猫 <i@nekomoe.xyz>
2024-12-31 14:17:54 +0800

    fix build script error

--- copy_wasm_assets.sh
+++ copy_wasm_assets.sh
@@ -1,3 +1,4 @@
+mkdir -p ./public/assets
 cp -r ./node_modules/tesseract-wasm/dist/tesseract-core.wasm ./public/assets/
 cp -r ./node_modules/tesseract-wasm/dist/tesseract-core-fallback.wasm ./public/assets/
 cp -r ./node_modules/tesseract-wasm/dist/tesseract-worker.js ./public/assets/
